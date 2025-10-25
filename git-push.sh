9dd542cbb8333499cbac8d0d7363611ca53428fb
ageyev <ageyev@gmail.com>
2025-10-25 18:08:29 +0300

    add git push script

--- git-push.sh
+++ git-push.sh
@@ -3,15 +3,18 @@ source ./.env;
 
 # git init 
 
-# add remote repository 
-# git remote remove origin # remove remote 'origin' repository 
+# > add remote repository
+# git remote remove origin # remove remote 'origin' repository
+#
 # git remote add gitlab "git@gitlab.com:${GIT_USER_NAME}/${APP_NAME}.git"
-# git remote add github "git@github.com:${GIT_USER_NAME}/${APP_NAME}.git" 
+#
+# git remote add github "git@github.com:${GITHUB_NAMESPACE}/${APP_NAME}.git"
+# git push --set-upstream github master # first push
 
 git config --local user.name "$GIT_USER_NAME";
 git config --local user.email "$GIT_USER_EMAIL";
 
-# git config --local -l
+#git config --local -l
 
 #docusaurus clear
 #npm run-script build && git add ./build && git commit -a -m "run build script"
@@ -19,7 +22,7 @@ git config --local user.email "$GIT_USER_EMAIL";
 ssh-add -D
 
 ssh-add "${GITHUB_KEY_PATH}";
-git push github --all 
+git push github
 
 #ssh-add "${GITLAB_KEY_PATH}"
 #git push gitlab --all
